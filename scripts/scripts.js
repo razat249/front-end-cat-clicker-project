caeae550e250aa2b2e63331c2e5afc24a9dba993
razat249 <razat249@gmail.com>
2015-11-05 19:52:27 +0530

    Premium version of cat clicker

---
+++ scripts/scripts.js
@@ -0,0 +1,56 @@
+
+$('#li1').click(function() {
+	$('.container').empty();
+	$('.container').append('<div id="a" ><h2>Pupu</h2><img src="img/cat1.jpg"><h1 id="1" ></h1></div>');
+	var i = 0;
+	var j = 0
+	$('#a').click(function(){
+		i++;
+		$('#1').text(i);
+	});
+});
+
+$('#li2').click(function() {
+	$('.container').empty();
+	$('.container').append('<div id="a" ><h2>Mimi</h2><img src="img/cat2.jpg"><h1 id="1" ></h1></div>');
+	var i = 0;
+	var j = 0
+	$('#a').click(function(){
+		i++;
+		$('#1').text(i);
+	});
+});
+
+
+$('#li3').click(function() {
+	$('.container').empty();
+	$('.container').append('<div id="a" ><h2>Sese</h2><img src="img/cat3.jpg"><h1 id="1" ></h1></div>');
+	var i = 0;
+	var j = 0
+	$('#a').click(function(){
+		i++;
+		$('#1').text(i);
+	});
+});
+
+$('#li4').click(function() {
+	$('.container').empty();
+	$('.container').append('<div id="a" ><h2>Neni</h2><img src="img/cat4.jpg"><h1 id="1" ></h1></div>');
+	var i = 0;
+	var j = 0
+	$('#a').click(function(){
+		i++;
+		$('#1').text(i);
+	});
+});
+
+$('#li5').click(function() {
+	$('.container').empty();
+	$('.container').append('<div id="a" ><h2>Piki</h2><img src="img/cat5.jpg"><h1 id="1" ></h1></div>');
+	var i = 0;
+	var j = 0
+	$('#a').click(function(){
+		i++;
+		$('#1').text(i);
+	});
+});
