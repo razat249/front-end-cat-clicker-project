188a5faf06f72b305dfaa4f11b71587e69b30fd8
razat249 <razat249@gmail.com>
2015-11-06 00:43:30 +0530

    Added some cool features and styles

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,23 +1,31 @@
+var count = {
+	'a': 0,
+	'b': 0,
+	'c': 0,
+	'd': 0,
+	'e': 0
+}
 
 $('#li1').click(function() {
 	$('.container').empty();
 	$('.container').append('<div id="a" ><h2>Pupu</h2><img src="img/cat1.jpg"><h1 id="1" ></h1></div>');
-	var i = 0;
-	var j = 0
+
+	$('#1').text(count.a);
+
 	$('#a').click(function(){
-		i++;
-		$('#1').text(i);
+		// i++;
+		count.a++;
+		$('#1').text(count.a);
 	});
 });
 
 $('#li2').click(function() {
 	$('.container').empty();
 	$('.container').append('<div id="a" ><h2>Mimi</h2><img src="img/cat2.jpg"><h1 id="1" ></h1></div>');
-	var i = 0;
-	var j = 0
+	$('#1').text(count.b);
 	$('#a').click(function(){
-		i++;
-		$('#1').text(i);
+		count.b++;
+		$('#1').text(count.b);
 	});
 });
 
@@ -25,32 +33,29 @@ $('#li2').click(function() {
 $('#li3').click(function() {
 	$('.container').empty();
 	$('.container').append('<div id="a" ><h2>Sese</h2><img src="img/cat3.jpg"><h1 id="1" ></h1></div>');
-	var i = 0;
-	var j = 0
+	$('#1').text(count.c);
 	$('#a').click(function(){
-		i++;
-		$('#1').text(i);
+		count.c++;
+		$('#1').text(count.c);
 	});
 });
 
 $('#li4').click(function() {
 	$('.container').empty();
 	$('.container').append('<div id="a" ><h2>Neni</h2><img src="img/cat4.jpg"><h1 id="1" ></h1></div>');
-	var i = 0;
-	var j = 0
+	$('#1').text(count.d);
 	$('#a').click(function(){
-		i++;
-		$('#1').text(i);
+		count.d++;
+		$('#1').text(count.d);
 	});
 });
 
 $('#li5').click(function() {
 	$('.container').empty();
 	$('.container').append('<div id="a" ><h2>Piki</h2><img src="img/cat5.jpg"><h1 id="1" ></h1></div>');
-	var i = 0;
-	var j = 0
+	$('#1').text(count.e);
 	$('#a').click(function(){
-		i++;
-		$('#1').text(i);
+		count.e++;
+		$('#1').text(count.e);
 	});
 });
